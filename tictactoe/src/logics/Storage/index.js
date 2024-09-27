b5f22a878d11390fac6ec4e6c5f6ba985970e2d5
Rodrigo Caero <44063898+u124253@users.noreply.github.com>
2024-09-27 13:00:06 +0200

    The progress is saved to local storage

--- tictactoe/src/logics/Storage/index.js
+++ tictactoe/src/logics/Storage/index.js
@@ -1,10 +1,10 @@
 export const saveGameToStorage = ({ board, turn}) => {
     //Guardar partida
-    window.localStorage.setItem('board',JSON.stringify(board))
+    window.localStorage.setItem('Board',JSON.stringify(board))
     window.localStorage.setItem('turn',turn)
 }
 export const resetGameStorage = () =>{
 
-    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('Board')
     window.localStorage.removeItem('turn')
 }
